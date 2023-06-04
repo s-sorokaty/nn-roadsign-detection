b1a03a70c0b5594a484600c9f25bf3246f258809
s-sorokaty <sorokaty0824@gmail.com>
2023-06-04 14:38:29 +0700

    clenup and iframe full size

--- client/src/views/Dashboard/RealtimePrediction/index.js
+++ client/src/views/Dashboard/RealtimePrediction/index.js
@@ -5,8 +5,8 @@ import React from "react";
 
 function RealtimePrediction() {
   return (
-    <Flex direction='column' pt={{ base: "120px", md: "75px" }} height="100%">
-      <iframe allow="camera;microphone" src="http://localhost:8080/"></iframe>
+    <Flex direction='column' pt={{ base: "120px", md: "75px" }} height="1000px">
+      <iframe height="100%" allow="camera;microphone" src="http://localhost:8080/"></iframe>
     </Flex>
   );
 }
