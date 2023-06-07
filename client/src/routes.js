01807928b6c787031932d8fd8b13eaae67e56708
s-sorokaty <sorokaty0824@gmail.ru>
2023-06-07 21:43:50 +0700

    update icons

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,8 @@
 import PhotoPrediction from "views/Dashboard/PhotoPrediction";
 import VideoPrediction from "views/Dashboard/VideoPrediction";
 import RealtimePrediction from "views/Dashboard/RealtimePrediction";
+import {Image} from "@chakra-ui/react"
+import {ViewIcon} from "@chakra-ui/icons"
 
 import {
   HomeIcon,
@@ -12,21 +14,21 @@ var dashRoutes = [
   {
     path: "/photo-prediction",
     name: "Распознавание на фотографии",
-    icon: <HomeIcon color="inherit" />,
+    icon: <Image src="https://cdn.icon-icons.com/icons2/510/PNG/512/image_icon-icons.com_50366.png" />,
     component: PhotoPrediction,
     layout: "/admin",
   },
   {
     path: "/video-prediction",
     name: "Распознавание на видео",
-    icon: <StatsIcon color="inherit" />,
+    icon: <Image src="https://w7.pngwing.com/pngs/375/969/png-transparent-computer-icons-video-cameras-video-icon-angle-rectangle-photography.png" />,
     component: VideoPrediction,
     layout: "/admin",
   },
   {
     path: "/realtime-prediction",
     name: "Распознавание в реальном времени",
-    icon: <CreditIcon color="inherit" />,
+    icon: <Image src="https://w7.pngwing.com/pngs/639/614/png-transparent-computer-icons-camera-camera-photography-rectangle-camera-icon-thumbnail.png" />,
     component: RealtimePrediction,
     layout: "/admin",
   },
